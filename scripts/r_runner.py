7eeb2b270abbe64c2939302276a738b68488242d
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-03-25 08:17:44 +0330

    fix(r_runner.py): updating r_runner.py for R lang

--- scripts/r_runner.py
+++ scripts/r_runner.py
@@ -1,23 +1,23 @@
 import os
 from scripts.docker_handler import DockerHandler
 
-class OctaveRunner:
+class RRunner:
     SOLUTION_FOLDER = os.path.abspath("solution")
     TEST_CASES_FOLDER = os.path.abspath("test_cases")
     DOCKER_EXEC = ["docker-compose", "exec", "-T", "octave-container"]
 
     @staticmethod
-    def compile_octave(asm_file):
-        # No compile needed for @Octave
+    def compile_R(asm_file):
+        # No compile needed for @R
         pass
 
     @staticmethod
-    def run_octave(m_file, input_file):
+    def run_R(r_file, input_file):
         input_path = f"/test_cases/in/{input_file}"
         
-        with open(os.path.join(OctaveRunner.TEST_CASES_FOLDER, "in", input_file), "r") as f:
+        with open(os.path.join(RRunner.TEST_CASES_FOLDER, "in", input_file), "r") as f:
             expected_input = "\n".join([line.strip() for line in f.readlines()])
 
-        command = OctaveRunner.DOCKER_EXEC + ["bash", "-c", f"echo \"{expected_input}\" | octave --no-gui {m_file}"]
+        command = RRunner.DOCKER_EXEC + ["bash", "-c", f"echo \"{expected_input}\" | Rscript {r_file}"]
         output, _ = DockerHandler.exec_command(command)
         return output
